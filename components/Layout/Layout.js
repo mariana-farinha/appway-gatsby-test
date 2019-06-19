d7818f2caf58f07e90b6ed08ea4f4a630f008744
Mariana <mary.farinha@gmail.com>
2019-06-19 18:06:05 +0200

    rempve: jquery+popper references. Material-UI FTW!

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,10 @@
-import css from "../../static/styles/scss/global.scss";
 import Navigation from "../Navigation/Navigation";
 
 const Layout = props => (
-  <div>
+  <React.Fragment>
     <Navigation />
     {props.children}
-    <script src="/static/scripts/jquery-3.0.0.slim.min.js" />
-    <script src="/static/scripts/popper-1.15.0.min.js" />
-  </div>
+  </React.Fragment>
 );
 
 export default Layout;

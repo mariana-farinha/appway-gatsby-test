5eb8f92c3711c71b4a96d105e85c4d7f30ea811f
Mariana <mary.farinha@gmail.com>
2019-06-19 18:06:58 +0200

    add: Material-UI styling to Navigation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,23 +1,38 @@
 import Link from "next/link";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/styles";
+import appwayLogo from "../../static/images/logos/appway-logo.png?resize&size=300";
 
-const Navigation = () => (
-  <div className="w-100 border-bottom border-light">
-    <div className="container">
-      <div className="row">
-        <div className="col-12 p-3">
-          <Link href="/">
-            <a>
-              <img
-                src="/static/images/appway-logo.png"
-                className="mw-100"
-                style={{ width: "6.5rem" }}
-              />
-            </a>
-          </Link>
-        </div>
-      </div>
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2)
+  }
+}));
+
+const Navigation = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <Container fixed>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Link href="/">
+              <a>
+                <img
+                  src={appwayLogo.src}
+                  className="mw-100"
+                  style={{ width: "6.5rem" }}
+                />
+              </a>
+            </Link>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navigation;
